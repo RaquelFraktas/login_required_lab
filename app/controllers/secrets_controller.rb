741b931c7d7c5cf653303ad4909e7c6b928af0fc
github username <github email address>
2021-07-06 22:32:13 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,23 @@
+class SecretsController < ApplicationController
+
+  before_action :require_login
+  skip_before_action :require_login, only: [:index]
+
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  private 
+  def require_login
+    redirect_to '/login' unless session.include? :user_id
+  end
+
+end
